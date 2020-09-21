bba3c23c859200cc925c9418893672b81d18b04d
radityakurnianto <raditya.kurnianto@gmail.com>
2020-09-21 10:25:33 +0700

    add regex helper class

---
+++ Sources/RWExtensionPack/Regex.swift
@@ -0,0 +1,20 @@
+//
+//  Regex.swift
+//  RWExtensionPack
+//
+//  Created by Raditya Kurnianto on 9/21/20.
+//
+
+import Foundation
+
+class Regex {
+    static func check(keyword: String, against pattern: String) -> Bool {
+        let searchRange = NSRange(location: 0, length: keyword.count)
+        var error: Error = nil
+        
+        let regex = try NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+        let match = regex.firstMatch(in: keyword, options: [], range: searchRange)
+        guard let matched = match else { return false }
+        return matched.numberOfRanges > 0
+    }
+}
