d2109e0a532beb3228a50016366c27567067fdc8
radityakurnianto <raditya.kurnianto@gmail.com>
2020-09-21 11:05:37 +0700

    fix implementation of Regex

--- Sources/RWExtensionPack/Regex.swift
+++ Sources/RWExtensionPack/Regex.swift
@@ -8,13 +8,17 @@
 import Foundation
 
 public class Regex: NSObject {
-    public static func check(keyword: String, pattern: String) -> Bool { 
-        let searchRange = NSRange(location: 0, length: keyword.count)
-        var error: Error = nil
-        
-        let regex = try NSRegularExpression(pattern: pattern, options: .caseInsensitive)
-        let match = regex.firstMatch(in: keyword, options: [], range: searchRange)
-        guard let matched = match else { return false }
-        return matched.numberOfRanges > 0
+    public static func check(keyword: String, pattern: String) -> Bool {
+        do {
+            let searchRange = NSRange(location: 0, length: keyword.count)
+            var error: Error = nil
+            
+            let regex = try NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+            let match = regex.firstMatch(in: keyword, options: [], range: searchRange)
+            guard let matched = match else { return false }
+            return matched.numberOfRanges > 0
+        } catch {
+            return false
+        }
     }
 }
