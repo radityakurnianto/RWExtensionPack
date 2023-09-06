704d3b05505926d98accd9786c713db77cfbb4d3
dicky suryo <dicky.suryo@detik.com>
2023-09-06 11:30:56 +0700

    - update minimum target - change swift version to 5.5.0

--- Tests/RWExtensionPackTests/RWExtensionPackTests.swift
+++ Tests/RWExtensionPackTests/RWExtensionPackTests.swift
@@ -1,15 +1,16 @@
 import XCTest
 @testable import RWExtensionPack
 
-final class RWExtensionPackTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RWExtensionPack().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
-}
+//final class RWExtensionPackTests: XCTestCase {
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(RWExtensionPack().text, "Hello, World!")
+//
+//    }
+//
+//    static var allTests = [
+//        ("testExample", testExample),
+//    ]
+//}
